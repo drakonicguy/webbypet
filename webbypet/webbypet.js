ba89ec531bfd890ae393a0220f3522004cd13fc9
drakonicguy <34401597+drakonicguy@users.noreply.github.com>
2023-03-26 21:42:01 +0200

    Add files via upload

--- webbypet/webbypet.js
+++ webbypet/webbypet.js
@@ -4,6 +4,10 @@ function randomNumber(max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max + 1));
 }
+var mouseover = false
+var mouseheld = false
+var x = 0
+var y = 0
 
 // put this in a seperate config file later
 var petImage = "pet.png"
@@ -12,33 +16,32 @@ var talkImage = "talk.png"
 // eventually walkimage will be split into 4 seperate ones, one for each diagonal movement
 var walkImage = "walk.png"
 
-// create HTML element
-const petelement = document.createElement("div");
-petelement.id = "webbypet";
-petelement.innerHTML = "pet"
-document.body.appendChild(petelement);
-var pet = document.getElementById("webbypet")
 // decide on an action
 async function decide() {
     // later set the loop condition to a toggle, so that users can choose to turn this thing off
     while (true) {
-        console.log("Deciding...")
-        await sleep(500)
-        var rand = Math.random()
-        if (rand < 0.1) {
-        learn()
-        console.log("I will learn")
-        }
-        else if (rand < 0.45) {
-        await randWalk()
-        console.log("I will walk")
-        // wait for animation to finish
-        await sleep(10000)
-        console.log("done")
+        if (!(mouseover && mouseheld)) {
+            console.log("Deciding...")
+            await sleep(500)
+            var rand = Math.random()
+            if (rand < 0.1) {
+            learn()
+            console.log("I will learn")
+            }
+            else if (rand < 0.45) {
+            randWalk()
+            console.log("I will walk")
+            // wait for animation to finish
+            await sleep(10000)
+            console.log("done")
+            }
+            else {
+            speak()
+            console.log("I will speak")
+            }
         }
         else {
-        speak()
-        console.log("I will speak")
+            await sleep(6)
         }
     }
 }
@@ -70,5 +73,22 @@ function learn(text) {
 }
 // END OF DECLARATIONS
 
+// create HTML element
+const petelement = document.createElement("div");
+petelement.id = "webbypet";
+petelement.innerHTML = "pet"
+document.body.appendChild(petelement);
+var pet = document.getElementById("webbypet")
+
+// event checkers
+onmousedown = (event) => {console.log("mouse down"); mouseheld = true};
+onmouseup = (event) => {console.log("mouse up"); mouseheld = false};
+
+pet.onmouseover = function(event) {
+    console.log("mouse hovering");
+    mouseover = true
+}
+
+// initiate behaviour
 decide()
 
